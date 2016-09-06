db788c7ed37c184e8867c01cd8b1f915fa46c871
Musa Hayta <musahayta@gmail.com>
2016-09-06 18:43:34 +0000

    Add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
+    has_many :comments
     has_attached_file :image, styles: { medium: "600x300#", small: "300x150>" }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
